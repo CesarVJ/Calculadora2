a9baf76024230764cfae590fb7ae639590245b47
cesar valdez jacinto <cvj991@gmail.com>
2020-05-24 11:19:13 -0500

    Se cambio el diseño de la calculadora

--- cliente.php
+++ cliente.php
@@ -1,6 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Calculadora2</title>
+</head>
+<body>
 <CENTER>       
-<h1>Calculadora</h1>
-<form action="cliente.php" method="get">
+<h1 id="titulo">Calculadora</h1>
+<form class="calculadora" action="cliente.php" method="get">
     Numero 1: <input type="number" name="a" required><br>
     Numero 2: <input type="number" name="b" required><br>
     Operacion: <select name="action">
@@ -10,7 +19,7 @@
         <option value="division">Dividir</option>
 
     </select><br><br>
-    <input type="submit" value="Calcular" style="width:300px; height:30px;">
+    <input id="btn-calcular" type="submit" value="Calcular">
 </form>
 
 <?php //OPERACION SUMA
@@ -18,6 +27,7 @@
         $calculadora= file_get_contents("http://localhost/Calculadora2/servicio.php?action=suma&a=". $_GET["a"]."&b=".$_GET["b"]);
         $calculadora = json_decode($calculadora, true);
         ?>
+        <div class="resultado">
         <h1>SUMA</h1>
         <table>
             <tr>
@@ -30,6 +40,7 @@
                 <td>Total: </td><td> <h3><?php echo $calculadora["total"] ?></h3></td>
             </tr>
         </table>
+        </div>
     <?php
     }
     #Operacion resta
@@ -37,6 +48,7 @@
         $calculadora= file_get_contents("http://localhost/Calculadora2/servicio.php?action=resta&a=". $_GET["a"]."&b=".$_GET["b"]);
         $calculadora = json_decode($calculadora, true);
         ?>
+        <div class="resultado">
         <h1>RESTA</h1>
         <table>
             <tr>
@@ -49,12 +61,14 @@
                 <td>Total: </td><td> <h3><?php echo $calculadora["total"] ?></h3></td>
             </tr>
         </table>
+        </div>
     <?php
     }
     if(isset($_GET["action"]) && $_GET["action"] == "multiplicacion"){
         $calculadora= file_get_contents("http://localhost/Calculadora2/servicio.php?action=multiplicacion&a=". $_GET["a"]."&b=".$_GET["b"]);
         $calculadora = json_decode($calculadora, true);
         ?>
+        <div class="resultado">
         <h1>MULTIPLICACION</h1>
         <table>
             <tr>
@@ -67,6 +81,7 @@
                 <td>Total: </td><td> <h3><?php echo $calculadora["total"] ?></h3></td>
             </tr>
         </table>
+        </div>
     <?php
     }
 
@@ -75,6 +90,7 @@
             $calculadora= file_get_contents("http://localhost/Calculadora2/servicio.php?action=division&a=". $_GET["a"]."&b=".$_GET["b"]);
             $calculadora = json_decode($calculadora, true);
             ?>
+            <div class="resultado">
             <h1>DIVISION</h1>
             <table>
                 <tr>
@@ -87,7 +103,10 @@
                     <td>Total: </td><td> <h3><?php echo $calculadora["total"] ?></h3></td>
                 </tr>
             </table>
+            </div>
         <?php
         }
 ?>
 </CENTER>
+</body>
+</html>
